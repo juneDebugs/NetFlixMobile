6f21f5029aa3c874420dea02400776d989b1df70
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 10:47:03 -0700

    created MoviesPage

---
+++ NetFlixMobile/Views/MoviesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NetFlixMobile.Views
+{
+    public partial class MoviesPage : ContentPage
+    {
+        public MoviesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
