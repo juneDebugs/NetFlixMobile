b4c0fd0a14968b2c753db23ff934c59fd24b99de
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 11:42:34 -0700

    finished MovieDetailsPage C# CB

--- NetFlixMobile/Views/MovieDetailsPage.xaml.cs
+++ NetFlixMobile/Views/MovieDetailsPage.xaml.cs
@@ -7,9 +7,25 @@ namespace NetFlixMobile.Views
 {
     public partial class MovieDetailsPage : ContentPage
     {
+        private MovieService _movieService = new MovieService();
+        private Movie _movie;
+
         public MovieDetailsPage()
         {
+            if
+                (movie == null)
+                throw new ArgumentNullException(nameof(movie));
+
+            _movie = movie;
+
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            BindingContext = await _movieService.GetMovie(_movie.Title);
+
+            base.OnAppearing();
         }
     }
 }
