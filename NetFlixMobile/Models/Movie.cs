bf3bf627f41c5450c91af8c93c6e8d2ac0661709
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 12:01:24 -0700

    finished Movie Model

---
+++ NetFlixMobile/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Dynamic;
+namespace NetFlixMobile.Models
+{
+    public class Movie
+    {
+        [JsonProperty("show_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("show_title")]
+        public string Title { get; set; }
+
+        [JsonProperty("release_year")]
+        public int ReleaseYear { get; set; }
+
+        [JsonProperty("poster")]
+        public string Poster { get; set; }
+
+        [JsonProperty("rating")]
+        public float Rating { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+    }
+}
