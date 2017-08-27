2420640762d5536e2c0335c2a8da9c593f8d6f24
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 11:02:07 -0700

    created OnSearching

--- NetFlixMobile/Views/MoviesPage.xaml.cs
+++ NetFlixMobile/Views/MoviesPage.xaml.cs
@@ -7,14 +7,33 @@ namespace NetFlixMobile.Views
 {
     public partial class MoviesPage : ContentPage
     {
+        private MovieService _service = new MovieService();
+
+        private BindableProperty IsSearchingProperty =
+            BindableProperty.Create("IsSearching", typeof(bool), typeof(MoviesPage), false);
+
+        public bool IsSearching
+        {
+            get
+            {
+                return (bool)GetValue(IsSearchingProperty);
+            }
+
+            set
+            {
+                SetValue(IsSearchingProperty, value);
+            }
+        }
+
+
         public MoviesPage()
         {
             InitializeComponent();
         }
 
-        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        void OnTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
         {
-            throw new ();
+            throw new ();fgf
         }
     }
 }
